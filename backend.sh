540b61e7805df462cbbc010c73a69e59e6350395
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-01 07:15:14 +0530

    Adding redirector for error

--- backend.sh
+++ backend.sh
@@ -1,25 +1,25 @@
-dnf module disable nodejs -y &>/tmp/expense.log
-dnf module enable nodejs:20 -y &>/tmp/expense.log
-dnf install nodejs -y &>/tmp/expense.log
+dnf module disable nodejs -y &>>/tmp/expense.log
+dnf module enable nodejs:20 -y &>>/tmp/expense.log
+dnf install nodejs -y &>>/tmp/expense.log
 
-useradd expense &>/tmp/expense.log
-cp backend.service /etc/systemd/system/backend.service &>/tmp/expense.log
+useradd expense &>>/tmp/expense.log
+cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
 
-rm -rf /app &>/tmp/expense.log
+rm -rf /app &>>/tmp/expense.log
 
-mkdir /app &>/tmp/expense.log
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>/tmp/expense.log
-cd /app &>/tmp/expense.log
-unzip /tmp/backend.zip &>/tmp/expense.log
+mkdir /app &>>/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
+cd /app &>>/tmp/expense.log
+unzip /tmp/backend.zip &>>/tmp/expense.log
 
-cd /app &>/tmp/expense.log
-npm install &>/tmp/expense.log
+cd /app &>>/tmp/expense.log
+npm install &>>/tmp/expense.log
 
 
-systemctl daemon-reload &>/tmp/expense.log
-systemctl enable backend &>/tmp/expense.log
-systemctl start backend &>/tmp/expense.log
+systemctl daemon-reload &>>/tmp/expense.log
+systemctl enable backend &>>/tmp/expense.log
+systemctl start backend &>>/tmp/expense.log
 
-dnf install mysql -y &>/tmp/expense.log
+dnf install mysql -y &>>/tmp/expense.log
 
-mysql -h 172.31.13.27 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>/tmp/expense.log
+mysql -h 172.31.13.27 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
