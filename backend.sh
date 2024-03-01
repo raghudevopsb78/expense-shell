347936bf615d81169c6f4249d477f8e1771304ab
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-01 06:46:55 +0530

    Changing the order of backend service file copy.

--- backend.sh
+++ backend.sh
@@ -3,6 +3,8 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
 useradd expense
+cp backend.service /etc/systemd/system/backend.service
+
 
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
@@ -12,7 +14,6 @@ unzip /tmp/backend.zip
 cd /app
 npm install
 
-cp backend.service /etc/systemd/system/backend.service
 
 systemctl daemon-reload
 systemctl enable backend
