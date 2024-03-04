dc4cdb708ec017875c9f93783521fc5eba3aa3c4
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-04 07:17:39 +0530

    Update code with DNS names

--- mysql.sh
+++ mysql.sh
@@ -16,7 +16,7 @@ systemctl start mysqld &>>$LOG
 Check_Status $?
 
 Print_Task_Heading "Setup MySQL Password"
-echo 'show databases' |mysql -h 172.31.14.7 -uroot -p${mysql_root_password} &>>$LOG
+echo 'show databases' |mysql -h mysql-dev.rdevops6a.online -uroot -p${mysql_root_password} &>>$LOG
 if [ $? -ne 0 ]; then
   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOG
 fi
